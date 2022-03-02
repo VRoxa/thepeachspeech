bb3efca495ac714813603177ce4178a8d8d74504
VRoxa <victor.roxa.r@gmail.com>
2022-03-02 23:59:29 +0100

    Implement language service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LanguageService } from './services/language.service';
 
 @Component({
   selector: 'app-root',
   template: `<router-outlet></router-outlet>`
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  
+  constructor(private languagrService: LanguageService) { }
+
+  ngOnInit() {
+    this.languagrService.init();
+  }
+}
