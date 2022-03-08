8700f2e792028a819950223dc1cdacdc0f3ab34e
VRoxa <victor.roxa.r@gmail.com>
2022-03-08 21:34:44 +0100

    Re-style language toggle button

--- src/app/components/common/language-toggle.component.ts
+++ src/app/components/common/language-toggle.component.ts
@@ -6,14 +6,17 @@ import { TranslateService } from '@ngx-translate/core';
   selector: 'peach-language-toggle',
   template: `
     <div class="container">
-      <button mat-icon-button 
-        (click)="toggle()"
-      >
-        <mat-icon class="icon">language</mat-icon>
-      </button>
       <img
-        [src]="checked ? 'assets/img/uk.png' : 'assets/img/sp.png'"
-        [alt]="checked ? 'UK' : 'SP'"
+        [ngStyle]="{'opacity': lang === 'es' ? '0.4' : '1'}"
+        (click)="toggle('en')"
+        src="assets/img/uk.png"
+        alt="UK"
+      >
+      <img
+      [ngStyle]="{'opacity': lang === 'en' ? '0.4' : '1'}"
+      (click)="toggle('es')"
+        src="assets/img/sp.png"
+        alt="SP"
       >
     </div>
 `,
@@ -22,44 +25,41 @@ import { TranslateService } from '@ngx-translate/core';
       display: flex;
       align-items: center;
       justify-content: center;
+      
+      img {
+        margin: 0 0.2rem;
+        cursor: pointer;
+      }
     }
     .icon {
       font-size: 1.8rem;
     }
+
+    .disabled {
+      opacity: 0.5;
+    }
   `]
 })
 
-export class LanguageToggleComponent implements OnInit {
-
-  // true -> English
-  // false -> Spanish
-  public checked!: boolean;
+export class LanguageToggleComponent {
 
   constructor(
     private router: Router,
     private translateService: TranslateService) { }
 
-  ngOnInit() {
-    const changeLanguage = (lang: string) => {
-      this.checked = lang === 'en';
-    }
-
-    changeLanguage(this.translateService.currentLang);
-    this.translateService.onLangChange.subscribe(({ lang }) => {
-      changeLanguage(lang);
-    });
+  public get lang() {
+    return this.translateService.currentLang;
   }
 
-  public toggle = () => {
-    const navigate = (fromEnglish: boolean) => {
-      const curr = fromEnglish ? 'en' : 'es';
-      const to = fromEnglish ? 'es' : 'en';
-
-      const { url } = this.router;
-      const targetUrl = url.replace(`/${curr}`, to);
-      this.router.navigate([targetUrl]);
+  public toggle = (lang: string) => {
+    if (lang === this.lang) {
+      return;
     }
 
-    navigate(this.checked);
+    // Navigate
+    const [to] = ['en', 'es'].filter(l => l === lang);
+    const { url } = this.router;
+    const targetUrl = url.replace(`/${this.lang}`, to);
+    this.router.navigate([targetUrl]);
   }
 }
