afc1adecfa7f7a8e1fd2b76c9b66b6f2ddb77ff1
VRoxa <victor.roxa.r@gmail.com>
2022-03-22 23:24:44 +0100

    Revert "Re-style language toggle button"
    This reverts commit 8700f2e792028a819950223dc1cdacdc0f3ab34e.

--- src/app/components/common/language-toggle.component.ts
+++ src/app/components/common/language-toggle.component.ts
@@ -6,17 +6,14 @@ import { TranslateService } from '@ngx-translate/core';
   selector: 'peach-language-toggle',
   template: `
     <div class="container">
-      <img
-        [ngStyle]="{'opacity': lang === 'es' ? '0.4' : '1'}"
-        (click)="toggle('en')"
-        src="assets/img/uk.png"
-        alt="UK"
+      <button mat-icon-button 
+        (click)="toggle()"
       >
+        <mat-icon class="icon">language</mat-icon>
+      </button>
       <img
-      [ngStyle]="{'opacity': lang === 'en' ? '0.4' : '1'}"
-      (click)="toggle('es')"
-        src="assets/img/sp.png"
-        alt="SP"
+        [src]="checked ? 'assets/img/uk.png' : 'assets/img/sp.png'"
+        [alt]="checked ? 'UK' : 'SP'"
       >
     </div>
 `,
@@ -25,41 +22,44 @@ import { TranslateService } from '@ngx-translate/core';
       display: flex;
       align-items: center;
       justify-content: center;
-      
-      img {
-        margin: 0 0.2rem;
-        cursor: pointer;
-      }
     }
     .icon {
       font-size: 1.8rem;
     }
-
-    .disabled {
-      opacity: 0.5;
-    }
   `]
 })
 
-export class LanguageToggleComponent {
+export class LanguageToggleComponent implements OnInit {
+
+  // true -> English
+  // false -> Spanish
+  public checked!: boolean;
 
   constructor(
     private router: Router,
     private translateService: TranslateService) { }
 
-  public get lang() {
-    return this.translateService.currentLang;
-  }
-
-  public toggle = (lang: string) => {
-    if (lang === this.lang) {
-      return;
+  ngOnInit() {
+    const changeLanguage = (lang: string) => {
+      this.checked = lang === 'en';
     }
 
-    // Navigate
-    const [to] = ['en', 'es'].filter(l => l === lang);
-    const { url } = this.router;
-    const targetUrl = url.replace(`/${this.lang}`, to);
-    this.router.navigate([targetUrl]);
+    changeLanguage(this.translateService.currentLang);
+    this.translateService.onLangChange.subscribe(({ lang }) => {
+      changeLanguage(lang);
+    });
+  }
+
+  public toggle = () => {
+    const navigate = (fromEnglish: boolean) => {
+      const curr = fromEnglish ? 'en' : 'es';
+      const to = fromEnglish ? 'es' : 'en';
+
+      const { url } = this.router;
+      const targetUrl = url.replace(`/${curr}`, to);
+      this.router.navigate([targetUrl]);
+    }
+
+    navigate(this.checked);
   }
 }
